8b0a2a9821530925b429b846d0c227312ca412a5
Leif Denby <leif@denby.eu>
2017-10-03 14:18:20 +0100

    Python lib compilation when netcdf c++ unavailable
    Use a set of dummy functions in place of routines which normally load
    fields from netcdf file (which are used by the CLI)

---
+++ src/file_io_dummy.cpp
@@ -0,0 +1,12 @@
+#include "blitz/array.h"
+#include "common.h"
+
+void load_mask(blitz::Array<bool,3> &maskext) {
+  printf("Error: When compiling the python interface to the cloud-identification library the netcdf c++ routines for loading data are not available");
+  exit(1);
+}
+
+void load_field(blitz::Array<short,3> &fieldext) {
+  printf("Error: When compiling the python interface to the cloud-identification library the netcdf c++ routines for loading data are not available");
+  exit(1);
+}
--- tests/test_ncfile.py
+++ tests/test_ncfile.py
@@ -7,61 +7,64 @@ import netCDF4
 import subprocess
 import numpy as np
 import os
+import warnings
 
 from base import BaseTestClass
 
 DO_CLEANUP = True
 
-class TestNCFile(BaseTestClass):
+EXECUTABLE_PATH = os.path.join(os.environ.get("BUILD_TEMP_DIR", "build"), "main")
+if not os.path.exists(EXECUTABLE_PATH):
+    warnings.warn("Couldn't find main executable, skipping tests for it")
+else:
+    class TestNCFile(BaseTestClass):
 
-    def run_classifier(self, data, mask):
-        def save_input():
-            nx, ny = data.shape
-            nz = 1
+        def run_classifier(self, data, mask):
+            def save_input():
+                nx, ny = data.shape
+                nz = 1
 
-            fh = netcdf_file("mask_field.nc", "w")
+                fh = netcdf_file("mask_field.nc", "w")
 
-            fh.createDimension("x", nx)
-            fh.createDimension("y", ny)
-            fh.createDimension("z", nz)
+                fh.createDimension("x", nx)
+                fh.createDimension("y", ny)
+                fh.createDimension("z", nz)
 
-            maskext = fh.createVariable("maskext", np.int16, ("x", "y", "z"))
-            maskext[:,:,0] = mask.astype(np.int16)
+                maskext = fh.createVariable("maskext", np.int16, ("x", "y", "z"))
+                maskext[:,:,0] = mask.astype(np.int16)
 
-            fieldext = fh.createVariable("fieldext", np.int16, ["x", "y", "z"])
-            # XXX: input must be saved as int16 for now, so we must rescale
-            d_range = data.max() - data.min()
-            if d_range == 0.0:
-                d_scaled = np.ones_like(data)
-            else:
-                d_scaled = (data - data.min())/(d_range)*np.iinfo(np.int16).max
-            fieldext[:,:,0] = d_scaled.astype(np.int16)
+                fieldext = fh.createVariable("fieldext", np.int16, ["x", "y", "z"])
+                # XXX: input must be saved as int16 for now, so we must rescale
+                d_range = data.max() - data.min()
+                if d_range == 0.0:
+                    d_scaled = np.ones_like(data)
+                else:
+                    d_scaled = (data - data.min())/(d_range)*np.iinfo(np.int16).max
+                fieldext[:,:,0] = d_scaled.astype(np.int16)
 
-            fh.close()
+                fh.close()
 
-        def delete_input():
-            if DO_CLEANUP:
-                os.remove("mask_field.nc")
+            def delete_input():
+                if DO_CLEANUP:
+                    os.remove("mask_field.nc")
 
-        def read_output():
-            fh = netCDF4.Dataset("output.nc")
-            d = fh.variables['data'][:][:,:,0]
-            fh.close()
-            if DO_CLEANUP:
-                os.remove("output.nc")
+            def read_output():
+                fh = netCDF4.Dataset("output.nc")
+                d = fh.variables['data'][:][:,:,0]
+                fh.close()
+                if DO_CLEANUP:
+                    os.remove("output.nc")
 
-            return d
+                return d
 
-        save_input()
-        executable_path = os.path.join(os.environ.get("BUILD_TEMP_DIR", "build"), "main")
-        if not os.path.exists(executable_path):
-            raise Exception("Couldn't find main executable")
+            save_input()
 
-        proc = subprocess.Popen(executable_path)
-        proc.communicate()
-        delete_input()
+            proc = subprocess.Popen(EXECUTABLE_PATH)
+            proc.communicate()
+            delete_input()
 
-        if not proc.returncode == 0:
-            raise Exception("classification program crashed, return code: {}".format(proc.returncode))
+            if not proc.returncode == 0:
+                raise Exception("classification program crashed, return code: {}".format(proc.returncode))
 
-        return read_output()
+            return read_output()
+
