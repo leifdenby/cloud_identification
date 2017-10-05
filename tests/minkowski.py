Fix bug in Minkowski calculation

tests/minkowski.py
@@ -157,19 +157,21 @@ def topological_scales(labels, dx):
     Compute characteristic thickness, width, length and genus using Minkowski
     functionals in 3D of all objects labelled objects in `labels`
     """
-    n0 = N0(labels).astype(float)
-    n1 = N1(labels).astype(float)
-    n2 = N2(labels).astype(float)
-    n3 = N3(labels).astype(float)
+    dtype = np.float32  # at the moment the cpp library uses only 32-bit precision
+    dx = dtype(dx)
+    n0 = N0(labels).astype(dtype)
+    n1 = N1(labels).astype(dtype)
+    n2 = N2(labels).astype(dtype)
+    n3 = N3(labels).astype(dtype)
 
     V0 = n3
     V1 = 2.0*(n2-3.0*n3)/(9.0*dx)
-    V2 = 2.0*(n1-2*n2+3*n3)/(9.0*dx*dx)
+    V2 = 2.0*(n1-2.0*n2+3.0*n3)/(9.0*dx*dx)
     V3 = (n0-n1+n2-n3)/(dx*dx*dx)
 
     thickness = np.abs(V0/(2.0*V1))
     width = np.abs(2.0*V1/(3.14159*V2))
-    length = np.abs(dx*dx*dx*3.0*V2/4.0)
+    length = np.abs(3.0*V2/(4.0*V3))
     genus = 1.0-0.5*(n0-n1+n2-n3)
 
     return np.array([thickness, width, length, genus])

tests/test_minkowski.py
@@ -11,6 +11,18 @@ def parse_data_str(s, shape):
 
 FUNCS = "N3 N2 N1 N0"
 
+def _assert_equal_floats(v1, v2):
+    assert v1.dtype == v2.dtype
+    eps = np.finfo(v1.dtype).eps
+    max_error = 1000*eps
+    print eps, max_error
+    print v1 - v2
+    print v1
+    print v2
+    if np.max(np.abs(v1 - v2) < max_error) > 100*eps:
+        warnings.warn("Larger than 100 epsilon descrepancy between c++ and python result, should investigate")
+    assert np.all(np.abs(v1 - v2) < max_error)
+
 def _check_func_values(test_data):
     m = test_data.get('mask')
 
@@ -34,7 +46,7 @@ def _check_func_values(test_data):
         dx = 25.0
         scales_py = scales_fn_py(m, dx)
         scales_cpp = scales_fn_cpp(m, dx)
-        assert np.all(np.abs(scales_py - scales_cpp) < 10*np.finfo(scales_cpp.dtype).eps)
+        _assert_equal_floats(scales_py, scales_cpp)
 
 def test_cube():
     test_data = dict(
@@ -204,22 +216,36 @@ def test_t_shape():
     )
     _check_func_values(test_data)
 
-def test_sphere():
-    l = 100.
-    N = 20
-    dx = l/N
+def make_spheroid_object(r0, dx):
+    if type(r0) == float:
+        lx = ly = lz = 4*r0
+        Nx = Ny = Nz = int(lx/dx)
+        r0x = r0y = r0z = r0
+    else:
+        if not type(r0) == np.ndarray:
+            r0 = np.array(r0)
+        r0x, r0y, r0z = r0
+        l = 4*r0
+        lx, ly, lz = l
+        Nx, Ny, Nz = (l/dx).astype(int)
 
-    x_ = np.linspace(-l/2., l/2., N)
-    y_ = np.linspace(-l/2., l/2., N)
-    z_ = np.linspace(-l/2., l/2., N)
+    x_ = np.linspace(-lx/2., lx/2., Nx)
+    y_ = np.linspace(-ly/2., ly/2., Ny)
+    z_ = np.linspace(-lz/2., lz/2., Nz)
 
     x, y, z = np.meshgrid(x_, y_, z_, indexing='ij')
 
-    r = np.sqrt(x**2. + y**2. + z**2.)
+    r = np.sqrt((x/r0x)**2. + (y/r0y)**2. + (z/r0z)**2.)
 
-    mask = r < 0.3*r.max()
+    mask = r < 1.0
 
-    labels = cloud_identification.number_objects(mask=mask, scalar_field=r)
+    return mask, r
+
+def test_sphere():
+    dx = 25.
+    mask, s = make_spheroid_object(r0=100., dx=dx)
+
+    labels = cloud_identification.number_objects(mask=mask, scalar_field=s)
 
     assert cloud_identification.N0(labels) == minkowski.N0(labels)
     assert cloud_identification.N1(labels) == minkowski.N1(labels)
@@ -230,9 +256,28 @@ def test_sphere():
     assert len(np.unique(labels)) == 2
 
     scales_cpp = cloud_identification.topological_scales(labels, dx=dx)
-    scales_py = minkowski.topological_scales(labels, dx=dx)
+    scales_py = minkowski.topological_scales(labels, dx=dx).astype(scales_cpp.dtype)
 
-    assert np.all(np.abs(scales_py - scales_cpp) < 1.0e2*np.finfo(scales_cpp.dtype).eps)
+    _assert_equal_floats(scales_py, scales_cpp)
+
+def test_ellipsoid():
+    dx = 25.
+    mask, s = make_spheroid_object(r0=[100., 400., 400.], dx=dx)
+
+    labels = cloud_identification.number_objects(mask=mask, scalar_field=s)
+
+    assert cloud_identification.N0(labels) == minkowski.N0(labels)
+    assert cloud_identification.N1(labels) == minkowski.N1(labels)
+    assert cloud_identification.N2(labels) == minkowski.N2(labels)
+    assert cloud_identification.N3(labels) == minkowski.N3(labels)
+
+
+    assert len(np.unique(labels)) == 2
+
+    scales_cpp = cloud_identification.topological_scales(labels, dx=dx)
+    scales_py = minkowski.topological_scales(labels, dx=dx).astype(scales_cpp.dtype)
+
+    _assert_equal_floats(scales_py, scales_cpp)
 
 if __name__ == "__main__":
     l = 100.